c26957b6748869e69608c7612b0e79d0eef61274
sceiiya <meetscheidj@gmail.com>
2025-02-26 20:50:09 +0800

    chore: add my install sh

---
+++ contracts/sceiiya-install.sh
@@ -0,0 +1,3 @@
+/Users/yesimscheidj/.foundry/bin/forge install foundry-rs/forge-std --no-commit --no-git
+/Users/yesimscheidj/.foundry/bin/forge install OpenZeppelin/openzeppelin-contracts-upgradeable --no-commit --no-git
+/Users/yesimscheidj/.foundry/bin/forge install OpenZeppelin/openzeppelin-foundry-upgrades --no-commit --no-git
