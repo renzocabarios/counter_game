3c37012a06232bb3c04775bd67e004dcbdd8eef8
sceiiya <meetscheidj@gmail.com>
2025-02-26 21:52:30 +0800

    update

--- ui/components/header.tsx
+++ ui/components/header.tsx
@@ -2,8 +2,8 @@ import ConnectWallet from "./common/connect-wallet";
 
 export default function Header() {
   return (
-    <div className="w-full flex sticky items-center justify-center p-5 border-b-2 border-gray-500">
-      <div className="w-full max-w-[1440px] flex items-center justify-between ">
+    <div className="sticky flex w-full items-center justify-center border-b-2 border-gray-500 p-5">
+      <div className="flex w-full max-w-[1440px] items-center justify-between">
         <p>CorePlay</p>
         <ConnectWallet />
       </div>
