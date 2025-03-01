ede875f740c714523bf8fb07883452928bba87b9
sceiiya <meetscheidj@gmail.com>
2025-03-01 18:21:35 +0800

    update: stats table

--- ui/components/statsTable.tsx
+++ ui/components/statsTable.tsx
@@ -14,19 +14,24 @@ function StatsTable({ data }: { data: any }) {
     <>
       <Table>
         <TableHeader>
-          <TableRow className="w-full text-white/100 justify-between">
-            <TableHead className="w-full">Time</TableHead>
-            <TableHead>User</TableHead>
-            <TableHead>Guess</TableHead>
+          <TableRow className="grid w-full grid-cols-3 gap-4 border-b py-2 text-white/100">
+            <TableHead className="text-start">Time</TableHead>
+            <TableHead className="text-center">User</TableHead>
+            <TableHead className="text-end">Guess</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {data && data.length > 0 ? (
             data.map((entry: any, i: number) => (
-              <TableRow className="" key={i}>
-                <TableCell>{formatTimeAgo(entry.time)}</TableCell>
-                <TableCell>{entry.user}</TableCell>
-                <TableCell>{entry.guess}</TableCell>
+              <TableRow
+                className="grid w-full grid-cols-3 gap-4 border-0 border-b py-2"
+                key={i}
+              >
+                <TableCell className="text-start">
+                  {formatTimeAgo(entry.time)}
+                </TableCell>
+                <TableCell className="text-center">{entry.user}</TableCell>
+                <TableCell className="text-end">{entry.guess}</TableCell>
               </TableRow>
             ))
           ) : (
