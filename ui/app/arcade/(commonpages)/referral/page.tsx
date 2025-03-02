d35f80625dca259ea06cf4aca6ce752696d154e8
sceiiya <meetscheidj@gmail.com>
2025-03-02 20:48:14 +0800

    chore: init common pages

---
+++ ui/app/arcade/(commonpages)/referral/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Background from "@/components/Background";
+
+export default function Referral() {
+  return (
+    <>
+      <Background />
+      <div className="flex w-full items-center justify-center">
+        <div className="flex w-full max-w-[1440px] flex-col gap-8 p-8"></div>
+      </div>
+    </>
+  );
+}
---
+++ ui/app/arcade/page.tsx
@@ -0,0 +1,12 @@
+import Background from "@/components/Background";
+
+export default function Arcade() {
+  return (
+    <>
+      <Background />
+      <div className="flex w-full items-center justify-center">
+        <div className="flex w-full max-w-[1440px] flex-col gap-8 p-8"></div>
+      </div>
+    </>
+  );
+}
