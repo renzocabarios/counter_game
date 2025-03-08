163de1c903cbf3a3b3b1a68f063c7ad8f8ca5169
sceiiya <meetscheidj@gmail.com>
2025-03-08 10:15:07 +0800

    debug: 1

--- ui/app/api/route.ts
+++ ui/app/api/route.ts
@@ -1,10 +1,9 @@
-import prisma from "@/lib/prisma";
-// import { Request, Response } from "next/server";
+// import prisma from "@/lib/prisma";
 
 export async function GET(req: Request) {
   try {
-    const users = await prisma.user.findMany();
-    return Response.json(users, {
+    // const users = await prisma.user.findMany();
+    return Response.json({"spi": "csll"}, {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
@@ -16,34 +15,49 @@ export async function GET(req: Request) {
   }
 }
 
-export async function POST(req: Request) {
-  try {
-    const email = "mongoloid@gmail.com";
-    const name = "bomm bakudan";
+// export async function GET(req: Request) {
+//   try {
+//     const users = await prisma.user.findMany();
+//     return Response.json(users, {
+//       status: 200,
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   } catch (error) {
+//     return Response.json(error, {
+//       status: 400,
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   }
+// }
 
-    try {
-      const user = await prisma.user.create({
-        data: {
-          email,
-          name,
-        },
-      });
-      return Response.json(user, {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      });
-    } catch (error) {
-      return Response.json(error, {
-        status: 400,
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-  } catch (error) {
-    return Response.json(error, {
-      status: 400,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
-}
+// export async function POST(req: Request) {
+//   try {
+//     const email = "mongoloid@gmail.com";
+//     const name = "bomm bakudan";
+
+//     try {
+//       const user = await prisma.user.create({
+//         data: {
+//           email,
+//           name,
+//         },
+//       });
+//       return Response.json(user, {
+//         status: 200,
+//         headers: { "Content-Type": "application/json" },
+//       });
+//     } catch (error) {
+//       return Response.json(error, {
+//         status: 400,
+//         headers: { "Content-Type": "application/json" },
+//       });
+//     }
+//   } catch (error) {
+//     return Response.json(error, {
+//       status: 400,
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   }
+// }
 
 export const revalidate = 0;
