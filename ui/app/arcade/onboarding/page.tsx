d7dee48812ceb488a8041229e8fffbc5b8f5c7d1
sceiiya <meetscheidj@gmail.com>
2025-03-08 09:14:11 +0800

    feat: add use params for referral id : sample url should be like this: http://localhost:3000/arcade/onboarding?referralid=k44Lkjr0

--- ui/app/arcade/onboarding/page.tsx
+++ ui/app/arcade/onboarding/page.tsx
@@ -6,6 +6,7 @@ import { useAccount } from "wagmi";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 export default function Page() {
   const { address, isConnected } = useAccount();
@@ -16,7 +17,19 @@ export default function Page() {
   const [isDisclaimerAccepted, setIsDisclaimerAccepted] = useState(false);
   const [username, setUsername] = useState<string>("");
   const [referrer, setReferrer] = useState<string>("");
+  const [isReferralValid, setIsReferralValid] = useState<boolean>(false); // Track if referral code is validated
   const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const searchParams = useSearchParams();
+  const referredByID = searchParams.get("referralid");
+  console.log("referredByID", referredByID);
+  const sampleRefIdList = [
+    "3F4lkjfi",
+    "3fglK55i",
+    "k44Lkjr0",
+    "9F7lk4fd",
+    "a54L3jfG",
+  ];
 
   const onboardingSteps = [
     { id: 1, step: "connect-wallet" },
@@ -26,6 +39,12 @@ export default function Page() {
     { id: 5, step: "disclaimer" },
     { id: 6, step: "welcome" },
   ];
+
+  useEffect(() => {
+    if (referredByID) {
+      setReferrer(referredByID);
+    }
+  }, [referredByID]);
 
   useEffect(() => {
     if (isConnected && !completedSteps.includes(1)) {
@@ -77,6 +96,17 @@ export default function Page() {
   const isUsernameValid = () => {
     const usernameRegex = /^[a-zA-Z0-9]{4,}$/;
     return usernameRegex.test(username);
+  };
+
+  const validateReferralCode = () => {
+    if (referrer && sampleRefIdList.includes(referrer)) {
+      setIsReferralValid(true);
+      return true;
+    } else {
+      setIsReferralValid(false);
+      alert("Invalid referral code. Please try again.");
+      return false;
+    }
   };
 
   const renderStepContent = () => {
@@ -133,7 +163,7 @@ export default function Page() {
                 placeholder="Name (min 4 characters only)"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
-                className="title h-[50px] w-full border-b border-green/100 bg-transparent p-4 text-white/50 placeholder-grey-100 focus:outline-none"
+                className="title h-[50px] w-full cursor-text border-b border-green/100 bg-transparent p-4 text-white/50 placeholder-grey-100 focus:outline-none focus:ring-0"
               />
               <p className="mt-2 text-sm text-green/100">
                 You can always change your name!
@@ -160,37 +190,39 @@ export default function Page() {
               <p className="subtitle text-left text-green/100">
                 Add profile pic
               </p>
-              <div
-                onClick={handleImageClick}
-                onDrop={handleDrop}
-                onDragOver={handleDragOver}
-                className="relative flex h-[200px] w-full cursor-pointer flex-col items-center justify-center rounded-md bg-white/100"
-              >
-                {!selectedImage ? (
-                  <>
+              <div className="flex w-full items-center justify-center">
+                <div
+                  onClick={handleImageClick}
+                  onDrop={handleDrop}
+                  onDragOver={handleDragOver}
+                  className={`relative flex h-[200px] cursor-pointer flex-col items-center justify-center rounded-md bg-white/100 ${selectedImage ? "w-[200px]" : "w-full"}`}
+                >
+                  {!selectedImage ? (
+                    <>
+                      <img
+                        src="/icons/upload.png"
+                        className="size-[128px]"
+                        alt="upload profile picture"
+                      />
+                      <p className="subtext pt-6 text-center text-black/100">
+                        Click here or Drag and Drop your Image
+                      </p>
+                    </>
+                  ) : (
                     <img
-                      src="/icons/upload.png"
-                      className="size-[128px]"
-                      alt="upload profile picture"
+                      src={selectedImage}
+                      alt="Profile preview"
+                      className="aspect-square h-full w-full rounded-md object-cover"
                     />
-                    <p className="subtext pt-6 text-center text-black/100">
-                      Click here or Drag and Drop your Image
-                    </p>
-                  </>
-                ) : (
-                  <img
-                    src={selectedImage}
-                    alt="Profile preview"
-                    className="aspect-square h-full w-full rounded-md object-cover"
+                  )}
+                  <input
+                    ref={fileInputRef}
+                    type="file"
+                    accept="image/*"
+                    onChange={handleFileInputChange}
+                    className="absolute inset-0 cursor-pointer opacity-0"
                   />
-                )}
-                <input
-                  ref={fileInputRef}
-                  type="file"
-                  accept="image/*"
-                  onChange={handleFileInputChange}
-                  className="absolute inset-0 cursor-pointer opacity-0" // Removed 'hidden'
-                />
+                </div>
               </div>
               <p className="subtitle mt-2 text-left text-green/100">
                 You can always change your picture!
@@ -215,22 +247,43 @@ export default function Page() {
             </p>
             <div>
               <p className="title text-green/100">Code</p>
-              <input
-                type="text"
-                value={referrer}
-                placeholder="Paste code here"
-                onChange={(e) => setReferrer(e.target.value)}
-                className="h-[82px] w-full rounded-md bg-white/10 p-4 text-white/100"
-              />
+              <div className="flex items-center justify-center gap-2">
+                <input
+                  type="text"
+                  value={referrer}
+                  placeholder="Paste code here"
+                  onChange={(e) => {
+                    setReferrer(e.target.value);
+                    setIsReferralValid(false);
+                  }}
+                  className="h-[82px] w-full cursor-text appearance-none rounded-md bg-transparent p-4 text-white/100 focus:outline-none focus:ring-0"
+                />
+                <Button
+                  onClick={validateReferralCode}
+                  className="title h-[30px] rounded-[8px] bg-white/100 text-black/100 hover:bg-white/80"
+                >
+                  Submit
+                </Button>
+              </div>
               <p className="mt-2 text-sm text-green/100">
-                Submit a friend code!
+                {isReferralValid
+                  ? "Referral code validated!"
+                  : "Submit a friend code!"}
               </p>
             </div>
             <Button
               onClick={() => handleContinue(4)}
+              disabled={!isReferralValid}
               className="title h-[82px] w-full bg-orange/100 text-white/100 hover:bg-orange/80"
             >
               Continue
+            </Button>
+            <Button
+              onClick={() => handleContinue(4)}
+              disabled={isReferralValid}
+              className="title bg-gray/100 hover:bg-gray/80 h-[82px] w-full text-white/100"
+            >
+              Skip
             </Button>
           </>
         );
@@ -253,7 +306,6 @@ export default function Page() {
                 participate.
               </p>
             </div>
-
             <div className="flex w-full items-center justify-center">
               <div
                 className={`mt-4 flex items-center justify-center gap-2 rounded-[6px] px-2 py-4 transition-colors duration-300 ${
